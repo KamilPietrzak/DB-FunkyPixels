67d678d40c0b5a1fa930f4e9eb42704869b26454
KamilPietrzak <kamilpietrzakpl@gmail.com>
2023-09-01 14:51:24 +0200

    feat: add the welcome message concept and the code for connecting to the server

---
+++ project/db_init.py
@@ -0,0 +1,36 @@
+from colorama import *
+from dotenv import load_dotenv
+import os
+import psycopg2 as db
+
+load_dotenv()
+
+just_fix_windows_console()
+init(autoreset=True)
+
+print(Fore.BLUE + """
+  /^ ^\\      
+ / 0 0 \\     ███████ ██    ██ ███    ██ ██   ██ ██    ██     ██████  ██ ██   ██ ███████ ██      ███████ 
+ V\ Y /V     ██      ██    ██ ████   ██ ██  ██   ██  ██      ██   ██ ██  ██ ██  ██      ██      ██      
+  / - \\      █████   ██    ██ ██ ██  ██ █████     ████       ██████  ██   ███   █████   ██      ███████ 
+ /    |      ██      ██    ██ ██  ██ ██ ██  ██     ██        ██      ██  ██ ██  ██      ██           ██ 
+V__) ||      ██       ██████  ██   ████ ██   ██    ██        ██      ██ ██   ██ ███████ ███████ ███████
+""")
+print("\n" + Fore.BLUE + "=======================================================================================================")
+print("\n" + Back.BLUE + Fore.BLACK + "Repozytory:" + Back.RESET + Fore.GREEN + " DB-FunkyPixels")
+print("\n" + Fore.BLUE + "=======================================================================================================")
+print("\n" + Back.MAGENTA + Fore.BLACK + "Email:" + Back.RESET + Fore.MAGENTA + " monetapietrzak@gmail.com")
+print("\n" + Back.MAGENTA + Fore.BLACK + "Website:" + Back.RESET + Fore.MAGENTA + " www.monetapietrzak.com")
+print("\n" + Fore.BLUE + "=======================================================================================================")
+
+try:
+    db.connect(host=os.getenv('HOST'), port=os.getenv('PORT'), user=os.getenv('USER'), password=os.getenv('PASSWORD'))
+except db.OperationalError as error:
+    if "password authentication failed" in error.args[0]:
+        print("Password or user authentication failed.")
+    elif "Unknown host" in error.args[0]:
+        print("Unknown host.")
+    elif "Connection refused" in error.args[0]:
+        print("Bad port.")
+    else:
+        print(error)
