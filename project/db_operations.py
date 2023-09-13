6c0e925dc62dcf76d9fa1d42d6b5250282bbcd6f
KamilPietrzak <kamilpietrzakpl@gmail.com>
2023-09-13 11:39:28 +0200

    feat: create achievement_notifications table (#28)

--- project/db_operations.py
+++ project/db_operations.py
@@ -410,6 +410,21 @@ class OperationsDatabase():
             self.con.rollback() # Back all changes.
             self.__close()
             raise InitTableError(error=error, table="administration_notifications")
+        
+        # Try to create the achievement_notifications table.
+        try:
+            self.cur.execute("""
+            CREATE TABLE IF NOT EXISTS achievement_notifications(
+	            id serial NOT NULL UNIQUE PRIMARY KEY,
+				user_id integer NOT NULL REFERENCES users(id),
+				achievement_id smallint NOT NULL REFERENCES achievements(id),
+	            displayed boolean NOT NULL DEFAULT False,
+				created timestamp NOT NULL DEFAULT NOW()
+            );""")
+        except Exception as error:
+            self.con.rollback() # Back all changes.
+            self.__close()
+            raise InitTableError(error=error, table="achievement_notifications")
 
     # The __close() method is used to close the cursor and connection with the database server.
     def __close(self):
